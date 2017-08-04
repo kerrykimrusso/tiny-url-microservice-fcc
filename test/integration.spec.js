d7b0bef80140d8005ccd7c21dc582b1362d4f5a4
System <none>
2017-08-04 06:30:09 +0000

    Checkpoint

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -1,9 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.promise = global.Promise; 
 const mocha = require('mocha');
 const before = mocha.before;
 const describe = mocha.describe;
+const beforeEach = mocha.beforeEach;
 const it = mocha.it;
 const chai = require('chai');
 const expect = chai.expect;
 const chaihttp = require('chai-http');
 chai.use(chaihttp);
 
+before(function(done) {
+  mongoose.connect(process.env.MONGODBURITEST);
+  mongoose.connection
+    .once('open', function() {
+        done();
+    })
+    .on('error', function(err) {
+        console.log(err);
+    });
+});
+
+describe('/new/http://google.com', function() {
+  beforeEach(function(done) {
+    mongoose.connection.db.dropCollection('tinyurl', function() {
+      console.log('tinyurl collection dropped');
+    });
+  });
+  
+});
