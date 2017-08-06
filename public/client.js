22d6b6fd0b5ace4027a7724990bc3b3ff3cb5ae7
System <none>
2017-08-06 04:47:47 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -1,13 +1,11 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
-$(function() {
-  $('form').submit(function(event) {
-    event.preventDefault();
-    window.location = "/new/" + 
+document.addEventListener('load', () => {
+  console.log('document loaded')
+  let form = document.querySelector('#tiny_url_form'); 
+  console.log()
+  form.addEventListener('submit', (e) => {
+    console.log('on submit')
+    e.preventDefault();
+    let url = document.querySelector('input[name="longUrl"]').val();
+    window.location = "/new/" + url;
   });
-
 });
