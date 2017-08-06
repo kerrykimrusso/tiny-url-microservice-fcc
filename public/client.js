732111db0489911f52b2af08ae023f09155804fd
System <none>
2017-08-06 04:53:47 +0000

    add form on index

--- public/client.js
+++ public/client.js
@@ -1,11 +1,11 @@
-document.addEventListener('load', () => {
+window.addEventListener('load', () => {
   console.log('document loaded')
   let form = document.querySelector('#tiny_url_form'); 
-  console.log()
   form.addEventListener('submit', (e) => {
-    console.log('on submit')
     e.preventDefault();
-    let url = document.querySelector('input[name="longUrl"]').val();
+    let longUrlInput = document.querySelector('#longUrl');
+    console.log(document.querySelector('#longUrl'));
+    let url = longUrlInput.value;
     window.location = "/new/" + url;
   });
 });
